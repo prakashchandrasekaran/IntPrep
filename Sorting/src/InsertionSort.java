52495c6a8ce11d5baaaed0e466cd8e519a27787e
Prakash Chandrasekaran <prakashc@outlook.com>
2017-07-14 18:38:24 -0700

    Sorting - Basic project setup

---
+++ Sorting/src/InsertionSort.java
@@ -0,0 +1,15 @@
+/**
+ * Created by prchand on 7/14/2017.
+ */
+public class InsertionSort
+{
+    public static void Main(String args[])
+    {
+
+    }
+
+    public void InsertionSort(int[] array)
+    {
+        
+    }
+}
