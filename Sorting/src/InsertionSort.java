9cb2206054a7e5607dae3ea58122eb155878933c
Prakash Chandrasekaran <prakashc@outlook.com>
2017-07-16 01:03:54 -0700

    Push insertion sort code

--- Sorting/src/InsertionSort.java
+++ Sorting/src/InsertionSort.java
@@ -3,13 +3,45 @@
  */
 public class InsertionSort
 {
-    public static void Main(String args[])
+    public static void main(String[] args)
     {
-
+        System.out.println("Insertion sort");
+        int[] array = new int[] {10,14,7,4,19,16};
+        InsertionSort(array, 6);
+        Print(array);
     }
 
-    public void InsertionSort(int[] array)
-    {
-        
+    public static void InsertionSort(int[] array, int n) {
+        if (n <= 1) {
+            return;
+        }
+
+        if (array == null) {
+            return;
+        }
+
+        if (n > array.length) {
+            n = array.length;
+        }
+
+        for (int i = 1; i < n; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while ((j >= 0) && (array[j] > current)) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current;
+        }
     }
+
+    public static void Print(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
